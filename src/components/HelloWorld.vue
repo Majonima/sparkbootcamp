d81788919246481cb4dce9432e087e265f0afb7f
stas <majonima@gmail.com>
2020-11-18 00:07:30 +0200

    test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+  <div class="box">
+    <p class="text">Груповий рейтинг</p>
+    <bars class="chart"
+          :data="Rating"
+          :label-data="Group"
+          :gradient="['#f94144', '#ffe74c']"
+          :barWidth="30"
+          :height="300"
+
+          :max="10"
+
+          label-rotate="0"
+          :growDuration="1"></bars>
+  </div>
+    <div class="box">
+      <p class="text">Завдань виконано</p>
+      <bars class="chart"
+            :data="Tasks"
+            :label-data="Group"
+            :gradient="['#f94144', '#ffe74c']"
+            :barWidth="30"
+            :height="300"
+
+            :max="10"
+            label-rotate="0"
+            :growDuration="1"></bars>
+    </div>
+
   </div>
 </template>
 
 <script>
+  import Bars from 'vuebars'
+  var groupData;
+  var rating = new Array();
+  var group = new Array();
+  var tasks = new Array();
 export default {
   name: 'HelloWorld',
+  components: {
+    Bars
+  },
   props: {
-    msg: String
+    Rating: Array,
+    Group:Array,
+    Tasks:Array,
+  },
+  mounted () {
+    groupData=new Array(this.getData());
+    var interval = setInterval(function() {
+      if (groupData[0][1]!=null){
+        console.log(groupData[0][1].FinalRating);
+        groupData[0].forEach(element => rating.push(element.FinalRating));
+        groupData[0].forEach(element => group.push(element.Group));
+        groupData[0].forEach(element => tasks.push(element.TaskReceived));
+        clearInterval(interval);
+      }
+    }, 500);
+    this.Rating=rating;
+    this.Group=group;
+    this.Tasks=tasks;
+  },
+
+  methods:{
+    getData(){
+      var finalRating= new Array();
+      database.ref('1P4D8W1bSUGsFw0-eZIZd3RhUBM432C7fw6CfUJIVNdk/Rating').once('value',function(querySnapshot) {
+        querySnapshot.forEach(function (snapshot) {
+          ratingData.push(snapshot.val())
+        });
+        //ratingData.forEach(element => finalRating.push(element.FinalRating))
+        ratingData.forEach(element => finalRating.push(element))
+
+      })
+      return finalRating;
+    }
   }
 }
+import firebase from "firebase";
+let ratingData = new Array();
+//let finalRatig = new Array();
+var config = {
+  apiKey: 'AIzaSyCGPNl0TuaHJR898UpNGbW5OBNjdTg6Vl4',
+  authDomain: "spark-bootcamp.firebaseapp.com",
+  databaseURL: "https://spark-bootcamp.firebaseio.com/",
+  projectId: 'spark-bootcamp'
+}
+
+firebase.initializeApp(config);
+var database= firebase.database();
+/*
+function addProp(key,value){
+  ratingData[key]=value;
+}
+*/
+
+
+
+
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -55,4 +118,47 @@ li {
 a {
   color: #42b983;
 }
+.box{
+  margin: 0;
+  padding: 0;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40vw;
+  height: 22vw;
+  overflow: visible;
+  color: white;
+  padding-bottom: 2vw;
+  display: inline-block;
+
+}
+  .chart{
+    margin: 0;
+    padding: 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 80%;
+    height: 90%;
+    overflow: inherit;
+  }
+  .text{
+    font-size: 3vw;
+    margin: 0;
+
+  }
+  .table{
+    font-size: 2vw;
+    margin: auto;
+    width: 80vw;
+    height: 90%;
+    border: white solid 4px;
+    border-radius: 20px;
+    border-collapse: collapse;
+    color: white;
+  }
+th, td {
+  padding: 15px;
+  border: white solid 2px;
+  border-collapse: inherit;
+}
+
 </style>
